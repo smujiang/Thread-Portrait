8da55135729b0cfb84a05ba11455c0f4e3cb9f79
Jun Jiang <smujiang@gmail.com>
2021-04-12 12:08:49 -0500

    Update Final picture.py

--- Final picture.py
+++ Final picture.py
@@ -1,58 +1,51 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
 
+def linea(a, b):
+    plt.plot([x[a], x[b]], [y[a], y[b]], "k", linewidth=0.1)
 
 
-def linea(a,b):
-	plt.plot([x[a],x[b]],[y[a],y[b]],"k",linewidth=0.1)
+img_dir = "/infodev1/non-phi-data/junjiang/OvaryCancer/toy_data"
+result_txt = os.path.join(img_dir, "result_txt.txt")
 
+with open(result_txt, "r") as file:
+    content = file.readlines()
 
-
-
-
-with open("lista de puntos.txt","r") as file:
-	content=file.readlines()
-
-content = [x.strip() for x in content] 
+content = [x.strip() for x in content]
 
 for i in range(len(content)):
-	content[i]=content[i]+" "
+    content[i] = content[i] + " "
 
-content[0:len(content)]= [''.join(content[0:len(content)])]
+content[0:len(content)] = [''.join(content[0:len(content)])]
 
-
-string=content[0].split(", ")
-string=string[:len(string)-1]
+string = content[0].split(", ")
+string = string[:len(string) - 1]
 for j in range(len(string)):
-	string[j]=int(string[j])
+    string[j] = int(string[j])
 
-lista=string
+lista = string
 
-n=306
-num_lineas=2500
-rad = 250*3
+n = 306
+num_lineas = 2500
+rad = 250 * 3
 cx = 0
 cy = 0
-actual=0
-angulo = np.linspace(0, 2*np.pi, n+1)
+actual = 0
+angulo = np.linspace(0, 2 * np.pi, n + 1)
 x = rad * np.cos(angulo) + cx
 y = rad * np.sin(angulo) + cy
 
-lista=lista
+lista = lista
 
-
-
-plt.plot(x, y, 'ro',markersize=1)
+plt.plot(x, y, 'ro', markersize=1)
 
 plt.gca().set_aspect('equal')
 
-for i in range(1,len(lista)):
-	b=lista[i]
-	linea(actual,b)
-	actual=b
+for i in range(1, len(lista)):
+    b = lista[i]
+    linea(actual, b)
+    actual = b
 
 plt.savefig("foto hilos")
-
-
-
